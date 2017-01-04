cb40b225efa7662ced82aad3a9b15e3d57ea487c
umar.abbas <omer.ithj@gmail.com>
2017-01-04 17:58:46 +0500

    add extra components at client side

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,15 @@
 ﻿import React from 'react';
 import ReactDom from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
 
-ReactDom.render(React.createElement('h1', null, 'Hello from React'), document.getElementById('react-app'));
+// remove tap delay, essential for MaterialUI to work properly
+injectTapEventPlugin();
+
+ReactDom.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>), document.getElementById('react-app'));
