d0bc22710aeeb5ba4ba68db49746b6df235788cc
umar.abbas <omer.ithj@gmail.com>
2017-01-10 22:53:39 +0500

    Base.jsx

---
+++ client/src/components/Base.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link,IndexLink} from 'react-router';
+
+const Base =({
+children
+})=>(
+    <div>
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <LinkIndex to="/">Yukoni</LinkIndex>
+            </div>
+            <div className="top-bar-right">
+                <Link to="/login">Log in</Link>
+                <Link to="/signup">Sign up</Link>
+            </div>
+        </div>
+        {children}
+    </div>
+);
+Base.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default Base;
