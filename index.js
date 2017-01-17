e77b276bb6843ad6393eea7c31ba669c50bbaba2
umar.abbas <omer.ithj@gmail.com>
2017-01-17 17:58:13 +0500

    Move config folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport =require('passport');
-const config =require('./server/config');
+const config =require('./config');
 
 require('./server/models').connect(config.dbUri);
 const app = express();
