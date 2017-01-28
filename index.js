c08977fe296856056b9f0e0544f9c5c6d16eed58
Umar Abbas <Umar Abbas>
2017-01-28 23:24:19 +0500

    bootify

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport =require('passport');
-const config =require('./config');
+const config =require('./server/config');
 
 require('./server/models').connect(config.dbUri);
 const app = express();
 // tell the app to look for static files in these directories
-app.use(express.static('./server/static/'));
+app.use(express.static('./server/views/'));
 app.use(express.static('./client/dist/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 // load passport strategies
-const localSignupStrategy = require('./server/passport/local-signup');
-const localLoginStrategy = require('./server/passport/local-login');
+const localSignupStrategy = require('./server/config/passport/local-signup');
+const localLoginStrategy = require('./server/config/passport/local-login');
 passport.use('local-signup', localSignupStrategy);
 passport.use('local-login', localLoginStrategy);
 
 // pass the authenticaion checker middleware
-const authCheckMiddleware = require('./server/middleware/auth-check');
+const authCheckMiddleware = require('./server/config/passport/auth-check');
 app.use('/api', authCheckMiddleware);
 
 // routes
